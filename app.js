aab852b5632727536336e0db795fca463cc8e9b5
Jason <jasylwong@gmail.com>
2020-06-12 00:08:37 +0100

    generate random for all 5 cards

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let numberOfCards = 1;
 const fewerCards = document.querySelector('#fewerCards')
 const moreCards = document.querySelector('#moreCards')
 const card1 = document.querySelector('#card1')
+const hand = document.querySelectorAll('#hand img')
 
 const cards = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
   "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
@@ -11,10 +12,10 @@ const cards = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC"
   "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"]
 
 button.onclick = function() {
-  const currentCard = cards[Math.floor(Math.random() * cards.length)]
-  card1.src = `./cards/${currentCard}.png`
-  card1.style = ""
-  console.log('card')
+  hand.forEach(card => {
+    const currentCard = cards[Math.floor(Math.random() * cards.length)]
+    card.src = `./cards/${currentCard}.png`
+  })
 }
 
 moreCards.onclick = function () {
