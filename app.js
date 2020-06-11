f986b3d6ccd04e561aeb463b4686af554423cd5e
Jason <jasylwong@gmail.com>
2020-06-11 23:54:32 +0100

    add buttons for more/fewer cards

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const button = document.querySelector('#button')
-const card = document.querySelector('#card')
+const numberOfCardsDisplay = document.querySelector('#numberOfCardsDisplay')
+let numberOfCards = 1;
+const fewerCards = document.querySelector('#fewerCards')
+const moreCards = document.querySelector('#moreCards')
+const card1 = document.querySelector('#card1')
 
 // const cards = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
 //   "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
@@ -15,7 +19,17 @@ const cards = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC"
 
 button.onclick = function() {
   const currentCard = cards[Math.floor(Math.random() * cards.length)]
-  card.src = `./cards/${currentCard}.png`
-  card.style = ""
+  card1.src = `./cards/${currentCard}.png`
+  card1.style = ""
   console.log('card')
+}
+
+moreCards.onclick = function () {
+  numberOfCards++;
+  numberOfCardsDisplay.innerHTML = numberOfCards;
+}
+
+fewerCards.onclick = function () {
+  numberOfCards--;
+  numberOfCardsDisplay.innerHTML = numberOfCards;
 }
