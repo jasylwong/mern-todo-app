ed52df63ec42e7a22a7efb775a201d8fc3158841
Jason <jasylwong@gmail.com>
2020-06-12 00:03:03 +0100

    link more/fewer cards button to actual cards

--- app.js
+++ app.js
@@ -5,17 +5,10 @@ const fewerCards = document.querySelector('#fewerCards')
 const moreCards = document.querySelector('#moreCards')
 const card1 = document.querySelector('#card1')
 
-// const cards = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
-//   "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
-//   "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
-//   "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"]
 const cards = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
   "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
   "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
   "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"]
-// const cards = ["AD", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC"]
-
-// const cards = ["AS"]
 
 button.onclick = function() {
   const currentCard = cards[Math.floor(Math.random() * cards.length)]
@@ -28,11 +21,13 @@ moreCards.onclick = function () {
   if(numberOfCards < 5) {
     numberOfCards++;
     numberOfCardsDisplay.innerHTML = numberOfCards;
+    document.querySelector(`#card${numberOfCards}`).removeAttribute('hidden');
   }
 }
 
 fewerCards.onclick = function () {
   if(numberOfCards > 1) {
+    document.querySelector(`#card${numberOfCards}`).setAttribute('hidden', true);
     numberOfCards--;
     numberOfCardsDisplay.innerHTML = numberOfCards;
   }
