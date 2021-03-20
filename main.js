adc97a77c54ab96adfc0f2254dca46bde772e760
jasylwong <jasylwong@gmail.com>
2021-03-20 18:30:37 +0000

    complete L6-event-handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       description: 'To keep your feet warm',
       image: './assets/images/socks_green.jpeg',
@@ -9,10 +10,21 @@ const app = Vue.createApp({
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpeg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg' }
       ],
       sizes: ['S', 'M', 'L']
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart > 0) this.cart -= 1 
+    },
+    updateImage(variantImage) {
+      this.image = variantImage
     }
   }
 })
