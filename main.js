51b979f8520d86a4e788ff8aecd3015237fc6d40
jasylwong <jasylwong@gmail.com>
2021-03-20 18:01:18 +0000

    complete L3-list-rendering

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ const app = Vue.createApp({
       description: 'To keep your feet warm',
       image: './assets/images/socks_green.jpeg',
       url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-      inventory: 0,
-      onSale: true
+      inventory: 20,
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: ['S', 'M', 'L']
     }
   }
 })
