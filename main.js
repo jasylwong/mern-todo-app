e604a7c656ccd24e0276f5b408b8eeb42be6be7f
jasylwong <jasylwong@gmail.com>
2021-03-20 17:40:39 +0000

    complete l3-attribute-binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'To keep your feet warm'
+      description: 'To keep your feet warm',
+      image: './assets/images/socks_green.jpeg',
+      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3'
     }
   }
 })
