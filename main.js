f568d19541da1d3266a957682734765dab36e65c
jasylwong <jasylwong@gmail.com>
2021-03-21 08:46:48 +0000

    complete L7: class & style binding

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const app = Vue.createApp({
       description: 'To keep your feet warm',
       image: './assets/images/socks_green.jpeg',
       url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-      inventory: 20,
+      inventory: 1,
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpeg' },
         { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg' }
       ],
-      sizes: ['S', 'M', 'L']
+      sizes: ['S', 'M', 'L'],
     }
   },
   methods: {
